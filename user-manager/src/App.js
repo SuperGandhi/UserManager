4838bddd833c7865fd290645ad22c67cd3c8d693
SuperGandhi <patrickborjabueno@hotmail.com>
2022-05-02 22:18:04 -0500

    adding component and css from card

--- user-manager/src/App.js
+++ user-manager/src/App.js
@@ -1,13 +1,16 @@
+import Card from './components/Card';
 import Input from './components/Input';
 import useFormulario from './hooks/useFormulario'
 
 function App() {
   const [formulario, handleChange] = useFormulario({name:'', lastname: ''})
   return (
+    <Card>
     <form>
       <Input label="Nombre: " name="name" value={formulario.name} onChange={handleChange}/>
       <Input label="Apellido: " name="lastname" value={formulario.lastname} onChange={handleChange}/>
     </form>
+    </Card>
   );
 }
 
