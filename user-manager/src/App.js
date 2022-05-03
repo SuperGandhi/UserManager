872cd7bbf830d3344a9d71416c055fe136eda880
SuperGandhi <patrickborjabueno@hotmail.com>
2022-05-02 22:25:25 -0500

    adding component and css container

--- user-manager/src/App.js
+++ user-manager/src/App.js
@@ -1,16 +1,21 @@
 import Card from './components/Card';
+import Container from './components/Container';
 import Input from './components/Input';
 import useFormulario from './hooks/useFormulario'
 
 function App() {
   const [formulario, handleChange] = useFormulario({name:'', lastname: ''})
   return (
+    <Container>
     <Card>
-    <form>
-      <Input label="Nombre: " name="name" value={formulario.name} onChange={handleChange}/>
-      <Input label="Apellido: " name="lastname" value={formulario.lastname} onChange={handleChange}/>
-    </form>
+      <div style={{padding: 20}}>
+        <form>
+          <Input label="Nombre: " name="name" value={formulario.name} onChange={handleChange}/>
+          <Input label="Apellido: " name="lastname" value={formulario.lastname} onChange={handleChange}/>
+        </form>        
+      </div>
     </Card>
+    </Container>
   );
 }
 
